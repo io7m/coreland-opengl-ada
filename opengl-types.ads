72f58089f3da3dfe96ee9135497dc1bd647c9206
m0 <m0@localhost>
2009-04-28 17:24:34 +0100

    update vector types.

--- opengl-types.ads
+++ opengl-types.ads
@@ -8,8 +8,9 @@ package OpenGL.Types is
   package Double_Arrays is new Ada.Numerics.Generic_Real_Arrays
     (Real => OpenGL.Thin.Double_t);
 
+  type Vector_2f_t is new Float_Arrays.Real_Vector (1 .. 2);
   type Vector_3f_t is new Float_Arrays.Real_Vector (1 .. 3);
-  type Vector_4f_t is new Float_Arrays.Real_Vector (1 .. 3);
+  type Vector_4f_t is new Float_Arrays.Real_Vector (1 .. 4);
 
   subtype Float_t          is Thin.Float_t;
   subtype Double_t         is Thin.Double_t;
