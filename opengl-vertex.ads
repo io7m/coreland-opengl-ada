43b4c0fffaebc144afa5f3766be67f66c5c35851
m0 <m0@viper.internal.network>
2009-10-31 21:26:13 +0000

    Add Vertex_*

--- opengl-vertex.ads
+++ opengl-vertex.ads
@@ -1,4 +1,5 @@
 with OpenGL.Thin;
+with OpenGL.Types;
 
 package OpenGL.Vertex is
 
@@ -23,6 +24,69 @@ package OpenGL.Vertex is
 
   -- proc_map : glEnd
   procedure GL_End renames Thin.GL_End;
+
+  -- proc_map : glVertex4f
+  procedure Vertex_4f
+    (X : in Types.Float_t;
+     Y : in Types.Float_t;
+     Z : in Types.Float_t;
+     W : in Types.Float_t)
+    renames Thin.Vertex_4f;
+
+  -- proc_map : glVertex3f
+  procedure Vertex_3f
+    (X : in Types.Float_t;
+     Y : in Types.Float_t;
+     Z : in Types.Float_t)
+    renames Thin.Vertex_3f;
+
+  -- proc_map : glVertex2f
+  procedure Vertex_2f
+    (X : in Types.Float_t;
+     Y : in Types.Float_t)
+    renames Thin.Vertex_2f;
+
+  -- proc_map : glVertex4d
+  procedure Vertex_4d
+    (X : in Types.Double_t;
+     Y : in Types.Double_t;
+     Z : in Types.Double_t;
+     W : in Types.Double_t)
+    renames Thin.Vertex_4d;
+
+  -- proc_map : glVertex3d
+  procedure Vertex_3d
+    (X : in Types.Double_t;
+     Y : in Types.Double_t;
+     Z : in Types.Double_t)
+    renames Thin.Vertex_3d;
+
+  -- proc_map : glVertex2d
+  procedure Vertex_2d
+    (X : in Types.Double_t;
+     Y : in Types.Double_t)
+    renames Thin.Vertex_2d;
+
+  -- proc_map : glVertex4i
+  procedure Vertex_4i
+    (X : in Types.Integer_t;
+     Y : in Types.Integer_t;
+     Z : in Types.Integer_t;
+     W : in Types.Integer_t)
+    renames Thin.Vertex_4i;
+
+  -- proc_map : glVertex3i
+  procedure Vertex_3i
+    (X : in Types.Integer_t;
+     Y : in Types.Integer_t;
+     Z : in Types.Integer_t)
+    renames Thin.Vertex_3i;
+
+  -- proc_map : glVertex2i
+  procedure Vertex_2i
+    (X : in Types.Integer_t;
+     Y : in Types.Integer_t)
+    renames Thin.Vertex_2i;
 
   --
   -- Map primitive types to enumerations.
